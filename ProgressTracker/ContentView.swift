cfd1864b1e09c45c3d8fb8f5cac3f85afd900bea
jooomin <joominchoi23@gmail.com>
2023-05-31 10:56:42 +0100

    first commit

--- ProgressTracker/ContentView.swift
+++ ProgressTracker/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Text("🏋️")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Progress Tracker")
         }
         .padding()
     }
