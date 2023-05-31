b47a99fdb48f560d937a2d275436d32b2ad41c7b
jooomin <joominchoi23@gmail.com>
2023-05-31 10:54:58 +0100

    Initial Commit

---
+++ ProgressTracker/ProgressTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressTrackerApp.swift
+//  ProgressTracker
+//
+//  Created by JooMin Choi on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProgressTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
